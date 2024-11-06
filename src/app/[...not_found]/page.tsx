3cd2c0f1b1527cb3c26718e86e6f323199b47446
grim <75869731+vys69@users.noreply.github.com>
2024-11-05 22:27:03 -0800

    escape apostrophe (fuck u eslint)

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -6,7 +6,7 @@ export default function Component() {
       <div className="w-full space-y-6 text-center">
         <div className="space-y-3">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl animate-bounce">404</h1>
-          <p className="text-gray-500">Looks like you've ventured into the unknown digital realm.</p>
+          <p className="text-gray-500">Looks like you&apos;ve ventured into the unknown digital realm.</p>
         </div>
         <Link
           href="/"
